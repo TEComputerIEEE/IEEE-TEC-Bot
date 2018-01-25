d7c9e7b72005bab9c46b265d884d0f158dfebef7
Malcolm Davis <me@malcolmdavis.xyz>
2018-01-25 14:32:06 -0600

    Comparations changed for the assert.

--- ieee_tec_bot/tests/test_connection.py
+++ ieee_tec_bot/tests/test_connection.py
@@ -16,7 +16,7 @@ def test_activities_get():
 	activities = conn.apiGet(config.activitiesEntryPoint)["activities"]
 	activity1 = conn.apiGet(config.activitiesEntryPoint, {"branchID":1})["activities"]
 	activity2 = conn.apiGet(config.activitiesEntryPoint, {"branchID":1, "chapterID":1})["activities"]
-	exist = len(activities) > 0 and len(activity1) > 0 and len(activity2) > 0
+	exist = activities != [] and activity1 !=[] and activity2 !=[]
 	assert exist
 
 '''
@@ -27,11 +27,11 @@ def test_activities_structure():
 	correct = isinstance(activity["activityID"], int) and activity["activityID"] > 0
 	correct &= isinstance(activity["name"], str) and len(activity["name"]) > 5
 	correct &= isinstance(activity["maxCapacity"], int) and activity["maxCapacity"] > 0
-	correct &= isinstance(activity["description"], str) and len(activity["description"]) > 0
-	correct &= isinstance(activity["place"], str) and len(activity["place"]) > 0
-	correct &= isinstance(activity["date"], str) and len(activity["date"]) > 0
+	correct &= isinstance(activity["description"], str) and activity["description"] != ""
+	correct &= isinstance(activity["place"], str) and activity["place"] != ""
+	correct &= isinstance(activity["date"], str) and activity["date"] != ""
 	correct &= isinstance(activity["users"], list)
-	correct &= isinstance(activity["flyer"], str) and len(activity["flyer"]) > 0
+	correct &= isinstance(activity["flyer"], str) and activity["flyer"] != ""
 	assert correct
 
 '''
@@ -50,7 +50,7 @@ def test_branch_structure():
 	branch = conn.apiGet(config.branchesEntryPoint, {"branchID":1})["branches"][0]
 	correct = isinstance(branch["branchID"], int) and branch["branchID"] > 0
 	correct &= isinstance(branch["college"], str) and len(branch["college"]) > 5
-	correct &= isinstance(branch["acronym"], str) and len(branch["acronym"]) > 0
+	correct &= isinstance(branch["acronym"], str) and branch["acronym"] != ""
 	assert correct
 
 
@@ -60,7 +60,7 @@ Test if the connection with the API to get contacts is working
 def test_contacts_get():
 	contacts1 = conn.apiGet(config.contactsEntryPoint, {"branchID":1})["contacts"]
 	contacts2 = conn.apiGet(config.contactsEntryPoint, {"branchID":1, "chapterID":1})["contacts"]
-	exist =  len(contacts1) > 0 and len(contacts2) > 0
+	exist =  contacts1 != [] and contacts2 != []
 	assert exist
 
 '''
@@ -69,9 +69,9 @@ Test if the format of the format of the returned contact is correct
 def test_contacts_structure():
 	activity = conn.apiGet(config.contactsEntryPoint,  {"branchID":1})["contacts"][0]
 	correct = isinstance(activity["contactID"], int) and activity["contactID"] > 0
-	correct &= isinstance(activity["name"], str) and len(activity["name"]) > 0
-	correct &= isinstance(activity["userName"], str) and len(activity["userName"]) > 0
-	correct &= isinstance(activity["role"], str) and len(activity["role"]) > 0
+	correct &= isinstance(activity["name"], str) and activity["name"] != ""
+	correct &= isinstance(activity["userName"], str) and activity["userName"] != ""
+	correct &= isinstance(activity["role"], str) and activity["role"] != ""
 	assert correct
 
 '''
@@ -80,7 +80,7 @@ Test if the connection with the API to get chapters is working
 def test_chapter_get():
 	chapters = conn.apiGet(config.chaptersEntryPoint, {"branchID":1})["chapters"]
 	chapter = conn.apiGet(config.chaptersEntryPoint, {"branchID":1, "chapterID":1})["chapters"]
-	exist = len(chapters) > 0 and len(chapter) > 0
+	exist = chapters !=[] and chapter !=[]
 	assert exist
 
 '''
@@ -98,7 +98,7 @@ Test if the connection with the API to get users is working
 '''
 def test_user_get():
 	user = conn.apiGet(config.usersEntryPoint, {"chatID":1})["users"]
-	exist = len(user) > 0 
+	exist = user != [] 
 	assert exist
 
 '''
@@ -106,8 +106,8 @@ Test if the format of the format of the returned user is correct
 '''
 def test_user_structure():
 	user = conn.apiGet(config.usersEntryPoint, {"chatID":1})["users"][0]
-	correct = isinstance(user["name"], str) and len(user["name"]) > 0
-	correct &= isinstance(user["studentID"], str) and len(user["studentID"]) > 0
+	correct = isinstance(user["name"], str) and user["name"]!= ""
+	correct &= isinstance(user["studentID"], str) and user["studentID"]!= ""
 	correct &= isinstance(user["email"], str) and len(user["email"]) > 5
 	assert correct
 
